192abac816bc9f4734bedd955e55d136d3957555
Gabriel Daniel Manea <gabimanea222@gmail.com>
2024-03-11 23:04:30 +0100

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,63 @@
+# Day 1 - 30DaysOfPython Challenge
+
+# Exercise 2.1 -----------------------------------------
+
+print(3 + 4)             # Addition(+)
+print(3 - 4)             # Subtraction(-)
+print(3 * 4)             # Multiplication(*)
+print(3 / 4)             # Division(/)
+print(3 ** 4)            # Exponential(**)
+print(3 % 4)             # Modulus(%)
+print(3 // 4)            # Floor division operator(//)
+
+# Exercise 2.2 -----------------------------------------
+
+print(type('Lorem Ipsum'))                    
+print(type('Dolor Sit Amet'))                   
+print(type('Consectetur'))
+
+# Exercise 2.3 -----------------------------------------
+
+# Checking data types
+print(type(10))                                 # Integer          
+print(type(9.8))                                # Float
+print(type(3.14))                               # Float
+print(type(4 - 4j))                             # Complex
+print(type(['Asabeneh','Python','Finland']))    # List
+print(type('Lorem Ipsum'))                      # String
+print(type('Dolor Sit Amet'))                   # String
+print(type('Consectetur'))                      # String
+
+# Exercise 3.1 ------------------------------------------
+
+print(type(22))
+print(type(16.66))
+print(type(1 + 2j))
+
+print(type("Hello World!"))
+
+print(type(True))
+
+print(type(['List','Of','Things']))
+
+print(type((2.7, 3.14, 6,23)))
+
+print(type({142, 213, 512}))
+
+print(type({'name':'Lorem Ipsum', 'age':'12'}))
+
+# Exercise 3.2 ------------------------------------------
+
+firstValue = 2;
+secondValue = 3;
+
+euclideanFormula = abs(firstValue - secondValue) # Used "abs()" to return the absolute number.
+
+print(euclideanFormula)
+
+firstValue = 10
+secondValue = 8
+
+euclideanFormula = abs(firstValue - secondValue) # Used "abs()" to return the absolute number.
+
+print(euclideanFormula)
